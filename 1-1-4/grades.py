c5f811c47b22e22fda39e6c50ea6f6bb3a7c63e1
The Cat <philip.birkland@vtsd.com>
2024-09-19 11:46:23 -0400

    Philip Birkland

---
+++ 1-1-4/grades.py
@@ -0,0 +1,12 @@
+grade = int(input("Enter the grade of the last test"))
+if grade > 89:
+    print("you got a A")
+elif grade > 79:
+    print("you got a B")
+elif grade > 69:
+    print("you got a C")
+elif grade > 64:
+    print("you got a D")
+elif grade > -1:
+    print("womp womp")
+
