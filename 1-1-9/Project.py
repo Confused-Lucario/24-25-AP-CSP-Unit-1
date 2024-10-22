f86854ce3d9ce5420a6f535b6163ed56866565c4
The Cat <philip.birkland@vtsd.com>
2024-10-22 13:55:08 -0400

    Philip Birkland jonathon frisco

--- 1-1-9/Project.py
+++ 1-1-9/Project.py
@@ -2,12 +2,13 @@ import turtle as t
 import turtle as Turtle
 t.colormode(255)
 t = t.Turtle()
-t.pensize(5)
+t.pensize(10)
+t_height = -400
 #canvase range is 800x800 square
 
 def wave(r,g,b):
     t.pencolor((r,g,b))
-    t.circle(575, -10)
+    t.circle(575,-10)
     t.pencolor("black")
 
 
@@ -15,14 +16,40 @@ def wave(r,g,b):
 
 t.penup()
 t.goto(400,-400)
-for Waves in range(1,10):
-    for waves in range(1,5):
+for waves in range(1,10):
+    for dwaves in range(1,5):
         t.pendown()
         wave(45,84,239)
         t.left(20)
         wave(45, 84, 239)
         t.left(t.heading())
-    t.goto(400,-400*)
+        t.penup()
+    t_height =+ 10
+    t.goto(400,t_height)
+    for lwave in range(1,5):
+        t.pendown()
+        wave(101,132,225)
+        t.left(20)
+        wave(101, 132, 225)
+        t.left(t.heading())
+        t.penup()
+    t_height =+ 10
+    t.goto(400,t_height)
+
+
+
+#Sun
+t.penup()
+t.goto(100,100)
+t.pendown()
+t.circle(10,10,10)
+t.color("yellow")
+
+#Snake
+t.penup()
+t.goto(50,0)
+t.forward(20)
+t.color("green")
 
 
 t.right(t.heading())
