9b9096b6344424f8ab33b8c892fe173df59ff9fb
The Cat <philip.birkland@vtsd.com>
2024-10-23 13:10:36 -0400

    Philip Birkland jonathon frisco

--- 1-1-9/Project.py
+++ 1-1-9/Project.py
@@ -1,22 +1,33 @@
-import turtle as t
+
 import turtle as Turtle
-t.colormode(255)
-t = t.Turtle()
+global t
+t = Turtle.Turtle()
+Turtle.colormode(255)
+t.speed(0)
+
 t.pensize(10)
-t_height = -400
 #canvase range is 800x800 square
 
 def wave(r,g,b):
+    global t
     t.pencolor((r,g,b))
-    t.circle(575,-10)
+    t.circle(575, extent=-10)
     t.pencolor("black")
 
 
 #Making the wave of the ocean
-
 t.penup()
-t.goto(400,-400)
-for waves in range(1,10):
+t.goto(0,-10000)
+t.pendown()
+t.begin_fill()
+t.color(195, 231, 245)
+t.circle(10000,360,360)
+t.end_fill()
+t.penup()
+ypos = -400
+t.goto(400, ypos)
+
+for waves in range(1,15):
     for dwaves in range(1,5):
         t.pendown()
         wave(45,84,239)
@@ -24,8 +35,8 @@ for waves in range(1,10):
         wave(45, 84, 239)
         t.left(t.heading())
         t.penup()
-    t_height =+ 10
-    t.goto(400,t_height)
+    ypos += 10
+    t.goto(400,ypos)
     for lwave in range(1,5):
         t.pendown()
         wave(101,132,225)
@@ -33,46 +44,66 @@ for waves in range(1,10):
         wave(101, 132, 225)
         t.left(t.heading())
         t.penup()
-    t_height =+ 10
-    t.goto(400,t_height)
+    ypos += 10
+    t.goto(400, ypos)
 
 
 
 #Sun
 t.penup()
 t.goto(100,100)
-t.pendown()
-t.circle(10,10,10)
 t.color("yellow")
-
-#Snake
+t.pendown()
+t.pensize(30)
+t.begin_fill()
+t.circle(120,360,360)
+t.end_fill()
+t.pensize(10)
 t.penup()
-t.goto(50,0)
-t.forward(20)
-t.color("green")
 
 
+
+#Sailboat
+t.goto(0,-100)
+t.color("brown")
+t.pendown()
+t.pensize(10)
+t.begin_fill()
+t.right(90)
+t.circle(50, 180, 360)
+t.end_fill()
+t.penup()
+t.goto(50,-100)
+t.pendown()
+t.color("brown")
+t.goto(50,-40)
+t.left(60)
+t.color(201, 205, 212)
+t.begin_fill()
+t.circle(30,360,3)
+t.end_fill()
+t.color("brown")
+t.goto(50,-100)
+
+#Border
+t.color("black")
 t.right(t.heading())
 t.penup()
-t.goto(400,400)
+t.goto(400,-400)
 t.pendown()
-for num in range(1,5):
-    t.forward(-800)
-    t.left(90)
+t.left(45)
+t.circle(565,360,4)
 
-num1 = 1
-num2 = 2
-#while num1 != num2:
-#    t.color
+
 
 wn = Turtle.Screen()
 wn.mainloop()
+
+
+
 '''
 blue wavy line, with different shades of blue
 yellow semicircle as the sun, centered above the wavy line
 fish scattered in the ocean
 few clouds
-thats it
-
-
 '''
